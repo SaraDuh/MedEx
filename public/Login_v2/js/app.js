8a90da9b50fbca816a42b5d468a718965a73dbf7
xxiMiaxx <lam3h-sa@live.co.uk>
2019-01-27 13:27:42 +0300

    adding signup

--- public/Login_v2/js/app.js
+++ public/Login_v2/js/app.js
@@ -1,10 +1,21 @@
 (function() {
 
+  var config = {
+    apiKey: "AIzaSyDQLxQEkY_lbpnQQi_JS8MXqCxgMIZr6eY",
+    authDomain: "medex-e6e1f.firebaseapp.com",
+    databaseURL: "https://medex-e6e1f.firebaseio.com",
+    projectId: "medex-e6e1f",
+    storageBucket: "medex-e6e1f.appspot.com",
+    messagingSenderId: "1006108228892"
+  };
+  firebase.initializeApp(config);
 // getting elements
 
 const txtEmail = document.getElementById('email');
 const txtPass = document.getElementById('password');
 const btnLogin = document.getElementById('btnLogin');
+const btnLogout = document.getElementById('btnLogOut');
+const btnSignUp = document.getElementById('btnSignUp');
 
 // add login event
 btnLogin.addEventListener('click', e => {
@@ -19,6 +30,22 @@ promise.catch(e => console.log(e.message));
 
 
 });
+
+//sign up function
+btnSignUp.addEventListener('click', e => {
+//get email and password
+const email = txtEmail.value;
+const password = txtPass.value;
+const auth = firebase.auth();
+
+// sign in
+const promise = auth.createUserWithEmailAndPassword(email, password);
+promise.catch(e => console.log(e.message));
+
+
+});
+
+
 // add realtime listener
 firebase.auth().onAuthStateChanged(firebaseUser => {
   if(firebaseUser){
@@ -28,5 +55,12 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
     console.log("not logged in");
   }
 });
+
+btnLogout.addEventListener('click', e => {
+firebase.auth().signOut();
+
+
+});
+
 
 }());
