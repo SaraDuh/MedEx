a7a3f3f743aecc7640cffe6cf6f6c352589fa807
xxiMiaxx <lam3h-sa@live.co.uk>
2019-03-23 19:06:39 +0300

    patient profile front end

--- public/userDashboard/fetching.js
+++ public/userDashboard/fetching.js
@@ -46,26 +46,46 @@ root.on("child_added", snap => {
   var Gender = snap.child("Gender").val();
   var age = snap.child("Age").val();
   var phone = snap.child("PhoneNo").val();
-  var HTMLtxt = '<tr><td class="serial">'+counter+'.</td><td id="MRN'+counter+'">'+MRN+'</td><td><span class="name">'+Name+'</span></td><td><span class="product">'+Gender+'</span></td><td><a href="#" onclick="patientProfile(\''+Name + '\',\''+ MRN + '\',\''+Gender +'\',\''+ age + '\',\''+ phone+'\')"><button type="button" class="btn btn-outline-primary btn-sm btn-outline-profile"><i class="fa fa-user"></i>&nbsp; Patient Profile</button></a></td></tr>';
+  var AddressNode = snap.child("Address");
+  var MH = snap.child("MedicalHistory").val();
+
+  var PodtalCode = AddressNode.child("Postal Code").val();
+  var Address = AddressNode.child("BuildingNo").val()+", "
+  +AddressNode.child("AdditionalNo").val()+", "
+  +AddressNode.child("Street Name").val()+", "
+  +AddressNode.child("Neighborhood").val()+", "
+  +AddressNode.child("City").val();
+  var HTMLtxt = '<tr><td class="serial">'+counter+'.</td><td id="MRN'+counter+'">'+MRN+'</td><td><span class="name">'+Name+'</span></td><td><span class="product">'+Gender+'</span></td><td><a href="#" onclick="patientProfile(\''+Name + '\',\''+ MRN + '\',\''+Gender +'\',\''+ age + '\',\''+ phone+ '\',\''+MH+'\',\''+Address+'\',\''+PodtalCode+'\')"><button type="button" class="btn btn-outline-primary btn-sm btn-outline-profile"><i class="fa fa-user"></i>&nbsp; Patient Profile</button></a></td></tr>';
 
    $("#tableBody").append(HTMLtxt);
  }
 });
 //<a href="/patientProfile.html?MRN='+MRN+'" onclick="patientProfile(\''+Name + '\',\''+ MRN + '\',\''+Gender +'\',\''+ age + '\',\''+ phone+'\')">
 //$("#btnEdit").click(function(){
-function patientProfile(Name, mrn, gender, age, phone){
+
+var P_mrn;
+function patientProfile(Name, mrn, gender, age, phone, MH, address, postalCode){
   console.log(age);
     $("div#patname h2").text(Name);
     $("li.li1").find('font').html(age);
     $("li.li2").find('font').html(gender);
     $("li.li3").find('font').html(mrn);
     $("li.li4").find('font').html(phone);
+    $("li.li5").find('font').html(MH);
+    $("li.li6").find('font').html(address);
+    $("li.li7").find('font').html(postalCode);
+    P_mrn=mrn;
   $("#patientLilProfile").show();
+
 
 
 
 }
 
+function goToForm(){
+  document.getElementById("a").href="../Prescription/AddPrescriptionForm.html?MRN="+P_mrn;
+
+}
 //$('button#changeText').click(function() {
 //   $('li.select2-selection__choice').find('font').html('HELLO WORLD');
 // });
